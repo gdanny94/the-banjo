65fcfa9f6769a91c4d057ae7480d65ccf84b8eb5
gdanny94 <109629790+gdanny94@users.noreply.github.com>
2024-11-04 18:59:14 -0500

    codewars

---
+++ main.js
@@ -0,0 +1,9 @@
+function areYouPlayingBanjo(name) {
+  
+  
+    return name[0].toLowerCase() === 'r' ? `${name} plays banjo` : `${name} does not play banjo`;
+  }
+  
+  console.log( areYouPlayingBanjo('Adam'))
+  console.log( areYouPlayingBanjo('Ron'))
+  console.log( areYouPlayingBanjo('ron'))
